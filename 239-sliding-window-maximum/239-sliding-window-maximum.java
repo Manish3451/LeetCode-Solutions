823851d7ba0fbeed47477d12085db3d42a1a0a3a
Manish Mandal <88921056+Manish3451@users.noreply.github.com>
2022-08-25 15:35:43 +0530

    Time: 63 ms (48.34%), Space: 150.4 MB (48.02%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -5,8 +5,8 @@ class Solution {
         int j = 0;
         Deque<Integer> q = new LinkedList<>();
         for (int i = 0; i < nums.length; i++) {
-            if (!q.isEmpty() && q.peekFirst() < i - k + 1) q.pollFirst();
-            while (!q.isEmpty() && nums[i] > nums[q.peekLast()]) q.pollLast();
+            if (!q.isEmpty() && q.peekFirst() < i - k + 1) q.pollFirst();//removing f
+            while (!q.isEmpty() && nums[i] > nums[q.peekLast()]) q.pollLast();//removing from the last
             q.offer(i);
             if (i >= k - 1) ans[j++] = nums[q.peekFirst()];
         }
