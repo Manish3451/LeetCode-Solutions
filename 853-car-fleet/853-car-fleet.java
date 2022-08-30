c5506442c524b4ceb381e138057848add6c25573
Manish Mandal <88921056+Manish3451@users.noreply.github.com>
2022-08-30 11:01:18 +0530

    Time: 135 ms (71.77%), Space: 83.7 MB (68.62%) - LeetHub

--- 853-car-fleet/853-car-fleet.java
+++ 853-car-fleet/853-car-fleet.java
@@ -1,24 +1,37 @@
+class Car{
+    int position;
+    double timetotarget;
+    
+    Car(int position,double timetotarget){
+        this.position = position;
+        this.timetotarget = timetotarget;
+    }
+}
+
 class Solution {
 
     public int carFleet(int target, int[] position, int[] speed) {
-        if (position.length == 1) return 1;
-        Stack<Double> stack = new Stack<>();
-        int[][] combine = new int[position.length][2];
-        for (int i = 0; i < position.length; i++) {
-            combine[i][0] = position[i];
-            combine[i][1] = speed[i];
+       
+        int n = speed.length;
+        Car cars[] = new Car[n];
+        if(n==0)return 0;
+        for(int i = 0; i < n; i++){
+            cars[i] = new Car(position[i],((target-position[i])*1.0)/speed[i]);
         }
-
-        Arrays.sort(combine, java.util.Comparator.comparingInt(o -> o[0]));
-        for (int i = combine.length - 1; i >= 0; i--) {
-            double currentTime = (double) (target - combine[i][0]) /
-            combine[i][1];
-            if (!stack.isEmpty() && currentTime <= stack.peek()) {
-                continue;
-            } else {
-                stack.push(currentTime);
+        Arrays.sort(cars,new Comparator<Car>(){
+            public int compare(Car a,Car b){
+                return a.position-b.position;
+            }
+        });
+        int ans = 0;
+        for(int i=n-1; i>=1; i--){
+            if(cars[i].timetotarget >= cars[i-1].timetotarget){
+                cars[i-1]=cars[i];
+            }
+            else{
+                ans++;
             }
         }
-        return stack.size();
+        return ans+1;
     }
 }
