0e9cc22813e71e295144b616c7d96a6203b84b79
Manish Mandal <88921056+Manish3451@users.noreply.github.com>
2022-12-15 12:11:47 +0530

    Added solution - LeetHub

--- Second Largest - GFG/second-largest.java
+++ Second Largest - GFG/second-largest.java
@@ -35,19 +35,17 @@ public class Main {
 class Solution {
     int print2largest(int arr[], int n) {
         // code here
-      int first = arr[0];
-      int second = -1;
-      
-      for(int i = 0; i < n; i++){
-          if(arr[i] > first){
-              second = first;
-              first = arr[i];
-          }
-          else if(arr[i] > second && arr[i] != first) {
-              second = arr[i];
-          }
-          
-      }
-      return second;
-}
+        int first = -1;
+        int second = arr[0];
+        for(int i = 0; i < n; i++){
+            if(arr[i]> first){
+                second = first;
+                first = arr[i];
+            }
+            else if(arr[i] > second && arr[i] != first){
+                second = arr[i];
+            }
+        }
+        return second;
+    }
 }
